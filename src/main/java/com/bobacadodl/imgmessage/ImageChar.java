064dc06d77b028c74f17bda1b74e8e734e3809dd
oko <oko403@protonmail.com>
2021-08-30 12:10:04 -0700

    simplify ImageMessage implementation

--- src/main/java/com/bobacadodl/imgmessage/ImageChar.java
+++ src/main/java/com/bobacadodl/imgmessage/ImageChar.java
@@ -6,11 +6,8 @@ package com.bobacadodl.imgmessage;
  * Time: 11:03 PM
  */
 public enum ImageChar {
-    BLOCK('\u2588'),
-    DARK_SHADE('\u2593'),
-    MEDIUM_SHADE('\u2592'),
-    LIGHT_SHADE('\u2591');
-    private char c;
+    BLOCK('\u2588');
+    private final char c;
 
     ImageChar(char c) {
         this.c = c;
